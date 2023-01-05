a23aa34c5d1c4ebf48370b7d15c0d58eb77b6313
BleedFeels <alfiopulvirenti18@gmail.com>
2023-01-05 12:16:28 -0500

    Nivelacion

---
+++ script.js
@@ -0,0 +1,8 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+document.ge
+
+open.addEventListener('click', () =>container.classList.add('show-naw') )
+
+close.addEventListener('click', () =>container.classList.add('show-naw') )
